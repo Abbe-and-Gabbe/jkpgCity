ba535332939438248cc4bbdf88bfe71aaa12a6e4
Gabriel <gedward359@gmail.com>
2024-02-23 10:09:48 +0100

    basic stuff

---
+++ frontend/src/Hamburger.svelte
@@ -0,0 +1,27 @@
+<script>
+    let isOpen = false;
+
+    function toggleMenu() {
+        isOpen = !isOpen;
+    }
+</script>
+
+<label for="toggler" class="hamburger" on:click={toggleMenu}>
+    <i class="ri-menu-line"></i>
+</label>
+
+<style>
+    .hamburger {
+        display: none;
+        cursor: pointer;
+        color: white;
+    }
+
+    @media screen and (max-width: 600px) {
+        .hamburger {
+            display: inline-flex;
+            align-items: center;
+            color: white;
+        }
+    }
+</style>
---
+++ frontend/src/Navbar.svelte
@@ -0,0 +1,71 @@
+<script>
+    import Hamburger from './Hamburger.svelte';
+</script>
+
+<nav class="navbar">
+    <a href="/" class="logo">LOGO</a>
+    <input type="checkbox" id="toggler">
+    <Hamburger />
+    <div class="menu">
+        <ul class="list">
+            <li><a href="/">ggg</a></li>
+            <li><a href="/">ggg</a></li>
+            <li><a href="/">ggg</a></li>
+        </ul>
+    </div>
+</nav>
+
+<style>
+    .navbar {
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: space-between;
+        background-color: black;
+        padding: 15px;
+    }
+
+    .navbar a{
+        text-decoration: none;
+        color: white;
+    }
+
+    .logo{
+        margin-right: 50px;
+    }
+
+    .list{
+        list-style: none;
+        display: flex;
+        gap: 25px;
+        color: blanchedalmond;
+    }
+
+    #toggler, .navbar label{
+        display: none;
+    }
+
+    @media screen and (max-width: 600px){
+        .menu{
+            width: 100%;
+            max-height: 0;
+            overflow: hidden;
+        }
+
+        .list{
+            flex-direction: column;
+            align-items: center;
+            padding: 20px;
+        }
+
+        .navbar label{
+            display: inline-flex;
+            align-items: center;
+            cursor: pointer;
+        }
+
+        #toggler:checked ~ .menu {
+            max-height: 100%;
+        }
+    }
+</style>
