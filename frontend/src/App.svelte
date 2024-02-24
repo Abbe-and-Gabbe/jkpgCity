9f741acd95048e1cdd23dc27713415964a358854
Gabriel <gedward359@gmail.com>
2024-02-24 21:58:06 +0100

    Attempt one

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,10 @@
 <script>
   import Navbar from './Components/Navbar.svelte';
   import Footer from './Components/Footer.svelte';
+  import Stores from './Stores/Stores.svelte';
 
 </script>
 
 <Navbar />
-
+<Stores/>
 <Footer/>
