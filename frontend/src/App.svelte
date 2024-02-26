2aa774cdb3fce135181be8d0e5dd69c047eb3872
Gabriel <gedward359@gmail.com>
2024-02-26 14:28:02 +0100

    Update routing

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,10 +1,18 @@
 <script>
   import Navbar from './Components/Navbar.svelte';
   import Footer from './Components/Footer.svelte';
-  import Stores from './Stores/Stores.svelte';
-
+  import Venues from './Venues/Venues.svelte';
+  import { Router, Link, Route } from "svelte-routing";
+  export let url = "";
 </script>
 
-<Navbar />
-<Stores/>
+<Router {url}>
+  <Navbar />
+  <main class="dark:bg-slate-900 bg-slate-100 dark:text-white p-12 shadow-xl h-full">
+    <Route path="/Venues">
+      <Venues/>
+    </Route>
+  </main>
+</Router>
 <Footer/>
+
