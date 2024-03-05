bb7f05f71ee827a5129b3ac9e4e0523dc7544cd5
salee <salee_wow@hotmail.se>
2024-03-05 14:43:28 +0100

    fix css source

--- frontend/src/Venues/Homepage.svelte
+++ frontend/src/Venues/Homepage.svelte
@@ -1,5 +1,32 @@
+<script>
 
+</script>
 
+<main class="flex flex-col flex-1 p-4">
+    <section id="pagetop" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
 
-<li><a href="">Alexxxx</a></li>
+        <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint ad consequuntur fugit iure officiis quaerat at eum voluptas incidunt reiciendis saepe, vitae, inventore laboriosam ullam exercitationem beatae distinctio! Libero, repudiandae.
+            </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam corrupti temporibus doloribus porro. Quos quo fugit perferendis qui, tempore possimus, placeat aliquid nihil modi deserunt labore iure quaerat illo nisi.
+            </p>
+            <a 
+                class="mx-auto lg:mr-auto text-base sm:text-lg md:text-xl relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950">
+
+            </a>
+        </div>
+
+        <div>
+
+        </div>
+
+        <div>
+            
+        </div>
+
+    </section> 
+</main>
+
 
