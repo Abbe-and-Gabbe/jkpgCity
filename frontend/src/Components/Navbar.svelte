e6cbbe0b8f68c457122eefeed820a3094bc66d2b
Gabriel <gedward359@gmail.com>
2024-03-05 14:20:51 +0100

    IMPchang

--- frontend/src/Components/Navbar.svelte
+++ frontend/src/Components/Navbar.svelte
@@ -7,7 +7,7 @@
   </script>
   
 
-<nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+<nav class="bg-black dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <span class="text-2xl font-semibold whitespace-nowrap dark:text-white ml-2">JKPGCITY</span> <!-- Added ml-2 class -->
       <button on:click={toggleNavbar} type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-expanded={isCollapsed}>
