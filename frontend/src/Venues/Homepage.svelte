401d08b868c4ae9c7e9c6e0180b5c8e876db0203
salee <salee_wow@hotmail.se>
2024-03-06 22:00:24 +0100

    mockup design homepage

--- frontend/src/Venues/Homepage.svelte
+++ frontend/src/Venues/Homepage.svelte
@@ -1,7 +1,17 @@
+<!-- @import url('https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap'); -->
+
+<script>
+    let isCollapsed = true; 
+  
+    function toggleNavbar() {
+      isCollapsed = !isCollapsed;
+    }
+  </script>
+
 
 <!--BOD
 <main class="bg-black flex justify-center items-center pt-200 min-h-4 pt-20"> -->
-
+  
 
 <body class="pt-16">
 
@@ -9,19 +19,39 @@
     <!--SECTION 1-->
     <section class="bg-blue-100 max-w-screen flex flex-wrap items-center justify-between mx-auto p-4 h-auto">
 
-        <img src="frontend/src/Venues/img/jkpgcity1.jpg" alt="city of jönköping">
-        <h1>Section 1</h1>
-        <p>Image and Title some text some text some text</p>
+        <!--<img src="jkpgcity1.jpg" alt="city of jönköping">-->
+        <div class="bg-black p-96 ml-24">
+
+        </div>
+
+        <div class="bg-black p-96 ml-24">
+            <h1 class="text-white">
+                Image and Title some text some text some text
+            </h1>
+        </div>
 
     </section>
 
 
     <!--SECTION 2-->
     <section class="bg-blue-300 max-w-screen flex flex-wrap items-center justify-between mx-auto p-4 h-auto">
-
+        <!-- 
         <img src="" alt="image 1">
         <img src="" alt="image 2">
         <img src="" alt="image 3">
+        -->
+        <div class="bg-black p-72 ml-24">
+
+        </div>
+
+        <div class="bg-black p-72 ml-24">
+
+        </div>
+
+        <div class="bg-black p-72 ml-24">
+
+        </div>
+        
 
     </section>
 
@@ -29,8 +59,20 @@
     <!--SECTION 3-->
     <section class="bg-blue-500 max-w-screen flex flex-wrap items-center justify-between mx-auto p-4 h-auto">
 
-        <h1>Öppettider</h1>
-        <p>Öppettider, some text some text, button</p>
+        <h1 class="w-full text-6xl p-2">
+            Öppettider
+        </h1>
+
+        <p class="w-full text-4xl p-2">Öppettider, some text some text, button</p>
+
+        <!--Från Gabbe-->
+        <div class="items-center justify-between w-full md:flex md:w-auto md:order-1">
+            <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <li>
+                  <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Klicka här --> </a>
+                </li>
+            </ul>
+        </div>
 
     </section>
 
@@ -39,7 +81,40 @@
     <section class="bg-blue-700 max-w-screen flex flex-wrap items-center justify-between mx-auto p-4 h-auto">
 
         <h1>Social Media</h1>
-        <p>Social media links, facebook, instagram, linkedin, tiktok</p>
+        <div class="bg-black p-8 text-white max-w-screen flex flex-wrap items-center justify-between mx-auto p-4 h-auto">
+            <li>
+                <span class="icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+                <a href="https://www.facebook.com/?stype=lo&deoia=1&jlou=AfcJKKgNokfOzUhAN76LtTFU21aWZPptE8T47Fe9R2Vw8YOB3-3Bg-oIMU_xIrM-kUREr6xyt8wpP_2RnR541U7BGl-T13pYMT909ZbjST1BBw&smuh=14569&lh=Ac8yNh_eHE-OuEJAK3Q" target="_blank">
+                    <span class="text">Facebook</span>
+                </a>
+            </li>
+
+            <li>
+                <span class="icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+                <a href="https://www.instagram.com/accounts/login/" target="_blank">
+                    <span class="text">Instagram</span>
+                </a>
+            </li>
+
+            <li>
+                <span class="icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+                <a href="#" target="_blank">
+                    <span class="text">Linkedin</span>
+                </a>
+            </li>
+
+            <li>
+                <span class="icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+                <a href="https://www.google.com/search?q=tiktok&rlz=1C1FHFK_svSE1025SE1025&oq=tiktk&gs_lcrp=EgZjaHJvbWUqDwgBEAAYChiDARixAxiABDIGCAAQRRg5Mg8IARAAGAoYgwEYsQMYgAQyDwgCEAAYChiDARixAxiABDIPCAMQABgKGIMBGLEDGIAEMg8IBBAAGAoYgwEYsQMYgAQyCQgFEAAYChiABDIPCAYQABgKGIMBGLEDGIAEMgwIBxAAGAoYsQMYgAQyEggIEAAYChiDARixAxiABBiKBTIMCAkQABgKGLEDGIAE0gEIMzAyMWowajeoAgCwAgA&sourceid=chrome&ie=UTF-8" 
+                target="_blank">
+                    <span class="text">TikTok</span>
+                </a>
+            </li>
+        </div>
+
+        <div class="border-t h-[1px] border-gray-400">
+
+        </div>
 
     </section>
 
@@ -47,7 +122,7 @@
     <!--SECTION 5-->
     <section class="bg-blue-900 max-w-screen flex flex-wrap items-center justify-between mx-auto p-4 h-auto">
 
-        <h1>JKPGCITY</h1>
+        <h1 class="w-full">JKPGCITY</h1>
         <ul>
             <li>Jönköping City AB</li>
             <li>c/o Jönköping kommun</li>
