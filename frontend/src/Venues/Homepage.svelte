9c1d5e53603ad9e44e6cb9f4661645c8709fc843
salee <salee_wow@hotmail.se>
2024-03-15 00:38:13 +0100

    Responsive and some, not done

--- frontend/src/Venues/Homepage.svelte
+++ frontend/src/Venues/Homepage.svelte
@@ -1,112 +1,121 @@
 
-
-<body class="pt-16 pb-16">
+<body class="dark:bg-gray-900 pt-1 pb-3">
 
 
     <!--SECTION 1-->
-    <section class="bg-blue-100 max-w-screen flex flex-wrap items-center justify-between mx-auto p-4 h-auto">
+    <section class="dark:bg-gray-900 grid gap-14 sm:grid-cols-12 m-14">
 
-        <img src="frontend/img/jkpgcity1.jpg" alt="city of jönköping">
-        <div class="bg-black p-96 ml-24">
+        <img src="img/jkpgcity1.jpg" alt="City of Jönköping" class="sm:col-span-4">
 
-        </div>
-
-        <div class="bg-black p-96 ml-24">
-            <h1 class="text-white">
-                Image and Title some text some text some text
-            </h1>
+        <div class=" dark:bg-gray-900 sm:col-span-8 flex items-center justify-center p-10">
+            <p class="max-w-lg text-3xl font-semibold leading-normal text-gray-900 dark:text-white">
+                The Al-powered app will help you improve yourself by analysing your everyday life.
+            </p>
         </div>
 
     </section>
-
 
     <!--SECTION 2-->
-    <section class="bg-blue-300 max-w-screen flex flex-wrap items-center justify-between mx-auto p-4 h-auto">
-        
-        <img src="/home/salee/project1/jkpgCity/frontend/img/jkpgcity1.jpg" alt="image 1">
-        <!-- 
-        lägga in bilder ist för div
-        -->
-        <div class="bg-black p-72 ml-24">
-        
-        </div>
+    <section class="dark:bg-gray-900 grid gap-14 sm:grid-cols-12 m-14">        
 
-        <div class="bg-black p-72 ml-24">
+        <img src="img/jkpgcity1.jpg" alt="City of Jönköping" class="sm:col-span-4 ">
 
-        </div>
+        <img src="img/jkpgcity1.jpg" alt="City of Jönköping" class="sm:col-span-4">
 
-        <div class="bg-black p-72 ml-24">
-
-        </div>
-        
+        <img src="img/jkpgcity1.jpg" alt="City of Jönköping" class="sm:col-span-4">
 
     </section>
 
-    
     <!--SECTION 3-->
-    <section class="bg-blue-700 max-w-screen flex flex-wrap items-center justify-between mx-auto p-4 h-auto">
+    <section class="dark:bg-gray-900 flex items-center justify-center m-14">
 
-        <h1>Social Media</h1>
-        <div class="bg-black p-8 text-white max-w-screen flex flex-wrap items-center justify-between mx-auto p-4 h-auto">
-            <li>
-                <span class="icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
-                <a href="https://www.facebook.com/?stype=lo&deoia=1&jlou=AfcJKKgNokfOzUhAN76LtTFU21aWZPptE8T47Fe9R2Vw8YOB3-3Bg-oIMU_xIrM-kUREr6xyt8wpP_2RnR541U7BGl-T13pYMT909ZbjST1BBw&smuh=14569&lh=Ac8yNh_eHE-OuEJAK3Q" target="_blank">
-                    <span class="text">Facebook</span>
-                </a>
-            </li>
+        <div class="dark:bg-gray-900">
 
-            <li>
-                <span class="icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
-                <a href="https://www.instagram.com/accounts/login/" target="_blank">
-                    <span class="text">Instagram</span>
-                </a>
-            </li>
-
-            <li>
-                <span class="icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
-                <a href="#" target="_blank">
-                    <span class="text">Linkedin</span>
-                </a>
-            </li>
-
-            <li>
-                <span class="icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
-                <a href="https://www.google.com/search?q=tiktok&rlz=1C1FHFK_svSE1025SE1025&oq=tiktk&gs_lcrp=EgZjaHJvbWUqDwgBEAAYChiDARixAxiABDIGCAAQRRg5Mg8IARAAGAoYgwEYsQMYgAQyDwgCEAAYChiDARixAxiABDIPCAMQABgKGIMBGLEDGIAEMg8IBBAAGAoYgwEYsQMYgAQyCQgFEAAYChiABDIPCAYQABgKGIMBGLEDGIAEMgwIBxAAGAoYsQMYgAQyEggIEAAYChiDARixAxiABBiKBTIMCAkQABgKGLEDGIAE0gEIMzAyMWowajeoAgCwAgA&sourceid=chrome&ie=UTF-8" 
-                target="_blank">
-                    <span class="text">TikTok</span>
-                </a>
-            </li>
+            <h2 class="text-3xl font-semibold text-center mb-6 text-gray-700">
+                Social Media
+            </h2>
+  
+            <div class="dark:bg-gray-900 flex flex-wrap justify-center gap-10">
+                   <!--Facebok-->
+                <button class="bg-blue-500 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                    <svg class="w-5 h-5 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" /></svg>
+                </button>
+          
+                <!--Twitter-->
+                <button class="bg-blue-400 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                    <svg class="w-5 h-5 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z" /></svg>
+                </button>
+  
+                    <!--Linkedin-->
+                <button class="bg-blue-600 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                    <svg class="w-5 h-5 fill-current" role="img" viewBox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
+                    <g><path d="M218.123122,218.127392 L180.191928,218.127392 L180.191928,158.724263 C180.191928,144.559023 179.939053,126.323993 160.463756,126.323993 C140.707926,126.323993 137.685284,141.757585 137.685284,157.692986 L137.685284,218.123441 L99.7540894,218.123441 L99.7540894,95.9665207 L136.168036,95.9665207 L136.168036,112.660562 L136.677736,112.660562 C144.102746,99.9650027 157.908637,92.3824528 172.605689,92.9280076 C211.050535,92.9280076 218.138927,118.216023 218.138927,151.114151 L218.123122,218.127392 Z M56.9550587,79.2685282 C44.7981969,79.2707099 34.9413443,69.4171797 34.9391618,57.260052 C34.93698,45.1029244 44.7902948,35.2458562 56.9471566,35.2436736 C69.1040185,35.2414916 78.9608713,45.0950217 78.963054,57.2521493 C78.9641017,63.090208 76.6459976,68.6895714 72.5186979,72.8184433 C68.3913982,76.9473153 62.7929898,79.26748 56.9550587,79.2685282 M75.9206558,218.127392 L37.94995,218.127392 L37.94995,95.9665207 L75.9206558,95.9665207 L75.9206558,218.127392 Z M237.033403,0.0182577091 L18.8895249,0.0182577091 C8.57959469,-0.0980923971 0.124827038,8.16056231 -0.001,18.4706066 L-0.001,237.524091 C0.120519052,247.839103 8.57460631,256.105934 18.8895249,255.9977 L237.033403,255.9977 C247.368728,256.125818 255.855922,247.859464 255.999,237.524091 L255.999,18.4548016 C255.851624,8.12438979 247.363742,-0.133792868 237.033403,0.000790807055"></path></g>
+                    </svg>
+                </button>
+  
+                <!--YouTube-->
+                <button class="bg-red-600 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                    <svg class="w-5 h-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z" /></svg>
+                </button>
+  
+                <!--Google-->
+                <button class="bg-red-500 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" class="w-5" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M12 0C5.372 0 0 5.373 0 12s5.372 12 12 12c6.627 0 12-5.373 12-12S18.627 0 12 0zm.14 19.018c-3.868 0-7-3.14-7-7.018c0-3.878 3.132-7.018 7-7.018c1.89 0 3.47.697 4.682 1.829l-1.974 1.978v-.004c-.735-.702-1.667-1.062-2.708-1.062c-2.31 0-4.187 1.956-4.187 4.273c0 2.315 1.877 4.277 4.187 4.277c2.096 0 3.522-1.202 3.816-2.852H12.14v-2.737h6.585c.088.47.135.96.135 1.474c0 4.01-2.677 6.86-6.72 6.86z" fill="currentColor"/></g></svg>
+                </button>
+            </div>
         </div>
-
-        <div class="border-t h-[1px] border-gray-400">
-
-        </div>
-
-    </section>
-
-
+    </section>  
+    
     <!--SECTION 4-->
-    <section class="bg-blue-900 max-w-screen flex flex-wrap items-center justify-between mx-auto p-4 h-auto">
+        <div class="dark:bg-gray-900 flex items-center justify-center m-14">
 
-        <h1 class="w-full">JKPGCITY</h1>
-        <ul>
-            <li>Jönköping City AB</li>
-            <li>c/o Jönköping kommun</li>
-            <li>Rådhusparken 1</li>
-            <li>551 89 Jönköping</li>
-            <br>
-            <li>Växel: 036-16 40 74</li>
-            <br>
-            <li>Besöksadress: Rådhuset, entreplan</li>
-            <a href="">
-                <p>hitta hit</p>
-            </a>
-            <br>
-            <li>info@jkpgcity.se</li>
-        </ul>
+            <div class="dark:bg-gray-900">
 
-    </section>
+                <h2 class="dark:bg-gray-900 mb-6 text-3xl font-bold ">
+                    JÖNKÖPING CITY
+                </h2>
 
+                <div class="dark:bg-gray-900">
+
+                    <p class="mb-6 text-neutral-500 dark:text-neutral-300">
+                        Jönköping City AB
+                    </p>
+
+                    <p class="mb-2 text-neutral-500 dark:text-neutral-300">
+                        c/o Jönköping kommun
+                    </p>
+
+                    <p class="mb-2 text-neutral-500 dark:text-neutral-300">
+                        Rådhusparken 1
+                    </p>
+            
+                    <p class="mb-2 text-neutral-500 dark:text-neutral-300">
+                     551 89 Jönköping
+                    </p>
+
+                    <p class="mb-2 text-neutral-500 dark:text-neutral-300">
+                        <br>
+                            Växel: 036-16 40 74
+                    </p>
+
+                    <p class="mb-2 text-neutral-500 dark:text-neutral-300">
+                        <br>
+                            Besöksadress: Rådhuset, entreplan
+                    </p>
+
+                    <p class="mb-2 text-neutral-500 dark:text-neutral-300">
+                        <a href="">
+                            <p>Hitta hit</p>
+                        </a>
+                    </p>
+
+                    <p class="mb-2 text-neutral-500 dark:text-neutral-300">
+                        <br>
+                            info@jkpgcity.se
+                    </p>
+                </div>
+            </div>
+        </div>
 
 
 </body>
