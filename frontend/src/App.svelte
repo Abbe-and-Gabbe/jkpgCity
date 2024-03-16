bdbb93dce945c17613f01323d57a766011255067
salee <salee_wow@hotmail.se>
2024-03-16 22:35:47 +0100

    responsive and design

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,11 +1,43 @@
 <script>
   import Navbar from './Components/Navbar.svelte';
   import Footer from './Components/Footer.svelte';
-  import Homepage from './Venues/Homepage.svelte';
+  import Homepage from './Homepage/Homepage.svelte';
+
+  import { Router, Link, Route } from "svelte-routing";
+  export let url = "";
+
+  
 
 </script>
+
+<!--
 
 <Navbar />
 <Homepage />
 <Footer />
 
+-->
+
+<Router {url}>
+  <Navbar />
+  <div class="mt-2.5">
+    <Route path="/Homepage" component={Homepage} />
+  </div>
+  <Footer />
+</Router>
+
+
+
+<!--
+
+
+  <Router {url}>
+  <Navbar/>
+  <div class="mt-2.5">
+    <Route path="/Homepage" component={Homepage} />
+  </div>
+  <Footer/>
+</Router>
+
+
+-->
